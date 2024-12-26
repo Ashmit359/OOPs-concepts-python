3acc27bede5fc8db8004fe9b9e9748ed768b654f
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-26 11:11:46 +0530

    oops concept

---
+++ class_&_object.py
@@ -0,0 +1,20 @@
+class student :
+    name = "Ashmit sinha"
+    def __init__(self):
+        print(self)
+        print("add new student in database")
+    
+s1 = student()
+print(s1)
+
+"""what does this self mean self means which is new  is the object which is being created
+i.e. this which  s1 object which we are creating as a new object, this is what we are
+calling self, self means myself,this is the reference of the object on whose creation the
+constructor is called,means it is pointing towards this,"""
+
+
+# class car :
+#     color = "blue"
+
+# car1 = car()
+# print(car1.color)    
