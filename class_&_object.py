3bfef5a8c7a0f97a7964a93761236816278f8c39
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-26 11:31:56 +0530

    add pdf and modified object , pass second arugment

--- class_&_object.py
+++ class_&_object.py
@@ -1,20 +1,16 @@
 class student :
-    name = "Ashmit sinha"
-    def __init__(self):
-        print(self)
+    def __init__(self,name, marks):
+        self.name = name
+        self.marks = marks
         print("add new student in database")
     
-s1 = student()
-print(s1)
+s1 = student("Ashmit sinha", 99)
+print(s1.name,s1.marks)
+
+s2 = student ("Anamika Sinha", 100)
+print(s2.name,s2.marks)
 
 """what does this self mean self means which is new  is the object which is being created
 i.e. this which  s1 object which we are creating as a new object, this is what we are
 calling self, self means myself,this is the reference of the object on whose creation the
 constructor is called,means it is pointing towards this,"""
-
-
-# class car :
-#     color = "blue"
-
-# car1 = car()
-# print(car1.color)    
